8215dc35ebcd45a2129194817d7e40f4aaea2481
Johannes van der Merwe <johannes.vandermerwe@adaptit.com>
2022-12-28 09:28:14 +0200

    add files

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./components/App"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter  basename="/projects/dalle-2-dashboard">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
